63437296b8707c232bebc5ec6eaca6e0a9d25122
AhmadIbrahim2 <ahmadribrahim0@gmail.com>
2024-01-10 22:09:40 +0400

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+var missingNumber = function (nums) {
+    const n = nums.length;
+    const expectedSum = (n * (n + 1)) / 2;
+    let actualSum = 0;
+  
+    for (let i = 0; i < nums.length; i++) {
+      actualSum = actualSum + nums[i]
+    }
+    
+    return expectedSum-actualSum
+  };
+
+  const nums = [3,0,1];
+  console.log(missingNumber(nums));
